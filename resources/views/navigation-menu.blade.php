ec02cc6bb9b4bafc6e00d3da313127a606defd0a
Daniel Held <danielxheld@gmail.com>
2023-03-26 06:15:22 +0200

    Bug-Fix of navigation routeIs()

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -145,7 +145,7 @@
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard*')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link href="{{ route('monitors') }}" :active="request()->routeIs('dashboard*')">
+            <x-responsive-nav-link href="{{ route('monitors') }}" :active="request()->routeIs('monitors*')">
                 {{ __('Monitors') }}
             </x-responsive-nav-link>
         </div>

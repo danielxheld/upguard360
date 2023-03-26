afb055748acc2c40a27e40465f502d71413c8f68
Daniel Held <danielxheld@gmail.com>
2023-03-26 06:09:09 +0200

    Added monitors navigation link to mobile view Added wildcard to routeIs()

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,12 +12,12 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard*')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link href="{{ route('monitors') }}" :active="request()->routeIs('monitors')">
+                    <x-nav-link href="{{ route('monitors') }}" :active="request()->routeIs('monitors*')">
                         {{ __('Monitors') }}
                     </x-nav-link>
                 </div>
@@ -142,8 +142,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard*')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('monitors') }}" :active="request()->routeIs('dashboard*')">
+                {{ __('Monitors') }}
             </x-responsive-nav-link>
         </div>
 
